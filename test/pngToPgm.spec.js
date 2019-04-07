3cc8d4bbd47e29d5404453b7ae7c3e45844d332d
Edward Compton <arkticpanda@gmail.com>
2019-04-07 16:15:39 +0100

    take average of all colours and default to 205 for unseen

--- test/pngToPgm.spec.js
+++ test/pngToPgm.spec.js
@@ -8,16 +8,25 @@ const pngToPgm = require('../lib/pngToPgm');
 
 const readFileAsync = util.promisify(fs.readFile);
 
-describe('pngToPgm', () => {
-    let testPGM, testPNG;
+describe('pngToPgm', function () {
+    let testPGM, testPNG, testPGM2, testPNG2;
+
+    this.timeout(5000); // Large files can take a long time to convert
 
     beforeEach(async () => {
         testPGM = await readFileAsync('./test/test_image.pgm');
         testPNG = await readFileAsync('./test/test_image.png');
+        testPGM2 = await readFileAsync('./test/test_image_2.pgm');
+        testPNG2 = await readFileAsync('./test/test_image_2.png');
     });
 
-    it('valid image', async () => {
+    it('test image 1', async () => {
         let result = await pngToPgm(testPNG);
         assert(result.equals(testPGM));
+    });
+
+    it('test image 2', async () => {
+        let result = await pngToPgm(testPNG2);
+        assert(result.equals(testPGM2));
     });
 });
